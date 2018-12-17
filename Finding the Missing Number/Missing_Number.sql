e574d73d866435da064c42d12fc1debf26528849
Timothy Mahajan <tmahajan@iu.edu>
2018-12-16 20:38:58 -0500

    Update Missing_Number.sql

--- Finding the Missing Number/Missing_Number.sql
+++ Finding the Missing Number/Missing_Number.sql
@@ -4,6 +4,9 @@
 declare @array varchar(max) =  '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25';
 declare @sql varchar(max) = '';
 declare @last int = len(@array) - len(replace(@array, ',', '')) + 2;
-with num as (select 1 as num, cast('1' as varchar(max)) as arr union all select num + 1, arr + '^' + cast((num + 1) as varchar(max)) from num where num < @last) 
-select @sql = 'select ' + replace(@array, ',', '^') + '^'  + (select arr from num where num = @last) option (maxrecursion 0);
+with num 
+as (select 1 as num, cast('1' as varchar(max)) as arr 
+union all select num + 1, arr + '^' + cast((num + 1) as varchar(max)) from num where num < @last) 
+select @sql = 'select ' + replace(@array, ',', '^') + '^'  + (select arr from num where num = @last) 
+option (maxrecursion 0);
 exec(@sql);
