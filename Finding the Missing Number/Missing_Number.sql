ac7a8bcbc2b1381b4bed198f7c491603aa2dbf74
Timothy Mahajan <tmahajan@iu.edu>
2018-12-16 20:42:28 -0500

    Update Missing_Number.sql

--- Finding the Missing Number/Missing_Number.sql
+++ Finding the Missing Number/Missing_Number.sql
@@ -4,7 +4,7 @@
 
 --In this solution we rely on the fact that one and only one number is missing out of a consecutive sequence of numbers, given in any order.
 --We have used a comma as a separator. The same principle would be applied to a larger list.
---We can also sum them instead of XORing them, but the latter uses less memory.
+--We can also sum them instead of XORing them, but the latter uses less operations.
 
 declare @array varchar(max) =  '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25';
 declare @sql varchar(max) = '';
